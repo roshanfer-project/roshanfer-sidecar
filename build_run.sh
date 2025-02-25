73b84b5e3eb10cbcdcaf585355c5d5caeb373c8b
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2025-02-25 19:23:07 +0000

    add helper script

--- build_run.sh
+++ build_run.sh
@@ -5,5 +5,11 @@ cd ./build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
 cmake --build .
 
+# Check if build was successful
+if [ $? -ne 0 ]; then
+    echo "Build failed"
+    exit 1
+fi
+
 # Run the sidecar
 GLOG_logtostderr=1 ./sidecar
