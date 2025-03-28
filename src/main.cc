8907755baf504599a3ddb23ec0f05c181cf8d3fa
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2025-03-28 15:35:41 +0000

    updating logging format and add signal support

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,20 @@
 #include <event_loop.h>
 #include <config.h>
 #include <iostream>
+#include <iomanip>
 #include <glog/logging.h>
+
+void MyPrefixFormatter(std::ostream& s, const google::LogMessage& m, void* /*data*/) {
+    s << google::GetLogSeverityName(m.severity())[0]
+    << ' '
+    << std::setw(2) << m.time().hour() << ':'
+    << std::setw(2) << m.time().min()  << ':'
+    << std::setw(2) << m.time().sec() << "."
+    << std::setw(6) << m.time().usec()
+    << ' '
+    << m.basename() << ':' << m.line() << "]";
+ }
+ 
 
 int main(int argc, char* argv[]) {
     // check one required argument
@@ -9,8 +22,11 @@ int main(int argc, char* argv[]) {
         std::cerr << "Usage: " << argv[0] << " <config_file>" << std::endl;
         return 1;
     }
+
     // init logging
     google::InitGoogleLogging(argv[0]);
+    google::InstallPrefixFormatter(&MyPrefixFormatter);
+    google::InstallFailureSignalHandler();
 
     Config config = load_config(argv[1]);
 
