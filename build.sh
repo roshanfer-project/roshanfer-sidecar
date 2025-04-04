1c54c402b1d68d27a14cf843bdaadc99888d8205
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2025-04-04 09:38:53 +0000

    switch to release mode when not profiling

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Ensure a build type is provided as an argument
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <build_type>"
+    exit 1
+fi
+
+BUILD_TYPE=$1
+echo "Build type set to: $BUILD_TYPE"
+
 # Build the sidecar
 cd ./build
-cmake ..   -DCMAKE_BUILD_TYPE=Debug
+cmake ..   -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cmake --build .
 
 # Check if build was successful
