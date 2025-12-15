76d264cc074f1fee1d5028d402fb37c547cc6e62
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2025-12-15 15:29:28 +0000

    update log levels

--- src/rpc_message.cc
+++ src/rpc_message.cc
@@ -126,7 +126,7 @@ void gRPCMessage::add_data(const uint8_t *data, size_t len, bool request) {
 }
 
 void gRPCMessage::clear() {
-  VLOG(2) << "Clearing gRPCMessage for ds_id: " << ds_stream_id
+  VLOG(3) << "Clearing gRPCMessage for ds_id: " << ds_stream_id
           << " ds_fd: " << ds_fd << " us_id: " << us_stream_id
           << " us_fd: " << us_fd;
   ds_stream_id = -1;
@@ -278,7 +278,7 @@ void HTTPMessage::add_data(const uint8_t *data, size_t len, bool request) {
 }
 
 void HTTPMessage::clear() {
-  VLOG(2) << "Clearing HTTPMessage for ds_id: " << ds_stream_id
+  VLOG(3) << "Clearing HTTPMessage for ds_id: " << ds_stream_id
           << " ds_fd: " << ds_fd << " us_id: " << us_stream_id
           << " us_fd: " << us_fd;
   ds_stream_id = -1;
