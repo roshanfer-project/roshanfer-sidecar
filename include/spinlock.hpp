4fc0430c07385eb05583c0bf98107901f0bb0945
Farzad Mohammadi <farzad.mohammadi87@gmail.com>
2025-12-19 17:38:01 +0000

    format

--- include/spinlock.hpp
+++ include/spinlock.hpp
@@ -3,16 +3,16 @@
 #include <atomic>
 
 class SpinLock {
-    public:
-        SpinLock() : locked() {}
-    public:
-        void lock() {
-            while (locked.test_and_set(std::memory_order_acquire)) {}
-        }
-        void unlock() {
-            locked.clear(std::memory_order_release);
-        }
-    
-    private:
-        std::atomic_flag locked;
+public:
+  SpinLock() : locked() {}
+
+public:
+  void lock() {
+    while (locked.test_and_set(std::memory_order_acquire)) {
+    }
+  }
+  void unlock() { locked.clear(std::memory_order_release); }
+
+private:
+  std::atomic_flag locked;
 };
